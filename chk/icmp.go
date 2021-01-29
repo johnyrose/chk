20368ba3bdc3f252fbd2551d5779ce9e22ba7c79
Jonathan Rosenboim <masterfqf@gmail.com>
2021-01-29 18:45:10 +0200

    Implement pinging

--- chk/icmp.go
+++ chk/icmp.go
@@ -1,5 +1,16 @@
 package chk
 
-func CheckICMP() error {
-	return nil
+import "github.com/go-ping/ping"
+
+func CheckICMP(address string, count int) (*ping.Statistics, error) {
+	pinger, err := ping.NewPinger(address)
+	if err != nil {
+		return nil, err
+	}
+	pinger.Count = count
+	err = pinger.Run()
+	if err != nil {
+		return nil, err
+	}
+	return pinger.Statistics(), nil
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,10 @@
 module github.com/Ripolak/chk
 
-
 go 1.15
 
 require (
+	github.com/go-ping/ping v0.0.0-20201115131931-3300c582a663 // indirect
 	github.com/urfave/cli/v2 v2.3.0
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 )
