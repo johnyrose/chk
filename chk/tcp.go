a89a8de085bee4322a6f221372d5f723ca8d9afc
Jonathan Rosenboim <masterfqf@gmail.com>
2021-01-30 12:55:21 +0200

    Add timeout check to TCP connection

--- chk/tcp.go
+++ chk/tcp.go
@@ -1,9 +1,13 @@
 package chk
 
-import "net"
+import (
+	"net"
+	"time"
+)
 
-func CheckTCP(address string) (net.Conn, error) {
-	conn, err := net.Dial("tcp", address)
+func CheckTCP(address string, timeout int) (net.Conn, error) {
+	d := net.Dialer{Timeout: time.Duration(timeout * int(time.Second))}
+	conn, err := d.Dial("tcp", address)
 	if err != nil {
 		return nil, err
 	}
--- cli/commands/tcp.go
+++ cli/commands/tcp.go
@@ -10,18 +10,27 @@ import (
 
 // TCPCommand represents a command in the CLI that checks TCP connection.
 var TCPCommand = &cli.Command{
-	Name:   "tcp",
-	Usage:  "Check if the target address is accessible via TCP.",
+	Name:  "tcp",
+	Usage: "Check if the target address is accessible via TCP.",
+	Flags: []cli.Flag{
+		&cli.IntFlag{
+			Name:    "timeout",
+			Aliases: []string{"t"},
+			Value:   1,
+			Usage:   "Time to wait for a response in seconds.",
+		},
+	},
 	Action: tcpAction,
 }
 
 func tcpAction(c *cli.Context) error {
 	address := c.Args().First()
 	err := validateArgument(address)
+	timeout := c.Int("timeout")
 	if err != nil {
 		return err
 	}
-	res, err := chk.CheckTCP(address)
+	res, err := chk.CheckTCP(address, timeout)
 	if err != nil {
 		displayTCPErrorResult(err, address)
 	} else {
